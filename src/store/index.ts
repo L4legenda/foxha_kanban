d3aa0cbd653db32dea26d8d9d2ba8d8d9767e079
l4legenda <l4legenda@gmail.com>
2022-04-05 22:16:30 +0500

    feat: base structure

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,79 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    _DEFAULT_BOARD: {
+      name: "Board #1",
+      tasks: []
+    },
+
+    _DEFAULT_TASK: {
+      name: "Task #1"
+    },
+
+    boards: [
+      {
+        name: "Board #1",
+        tasks: [
+          {
+            name: "Task #1"
+          },
+          {
+            name: "Task #2"
+          },
+          {
+            name: "Task #3"
+          },
+          {
+            name: "Task #4"
+          }
+        ]
+      },
+      {
+        name: "Board #2",
+        tasks: [
+          {
+            name: "Task #1"
+          },
+          {
+            name: "Task #2"
+          },
+          {
+            name: "Task #3"
+          },
+          {
+            name: "Task #4"
+          }
+        ]
+      }
+    ]
+  },
+  getters: {
+    boards(state){
+      return state.boards;
+    } 
   },
   mutations: {
+    insertBoard(state){      
+      state.boards.push({
+        name: "Board #1",
+        tasks: []
+      });
+    },
+    insertTask(state, keyBoard){
+      state.boards[keyBoard].tasks.push({
+        name: "Task #1"
+      });
+    }
   },
   actions: {
+    insertBoard(context){
+      context.commit("insertBoard");
+    },
+    insertTask(context, keyBoard){ 
+      console.log(keyBoard);
+      
+      context.commit("insertTask", keyBoard);
+    }
   },
   modules: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,154 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h2 class="title">Название Бокса</h2>
+    <p class="description">Описание Бокса</p>
+    <div class="board__list">
+      <div class="board__item" v-for="(board, board_key) in boards" :key="board_key">
+        <h3 class="board__title">{{ board?.name }}</h3>
+        <draggable class="draggable" :list="board.tasks" item-key="id" group="tasks">
+          <template #item="{ element }">
+            <div class="task">
+              <div class="marker__list">
+                <div class="marker__item">Hard Work</div>
+              </div>
+              <h3 class="task__title">{{ element.name }}</h3>
+              <div class="bottom__content">
+                <div class="info"></div>
+                <div class="users__list"></div>
+              </div>
+            </div>
+          </template>
+        </draggable>
+
+        <div class="board__bottom">
+          <button @click="insertTask(board_key)" class="board__link">Добавить задачу</button>
+        </div>
+      </div>
+      <div class="box__right">
+        <div class="box__add" @click="insertBoard()">+ Добавить доску</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { mapGetters, mapActions } from 'vuex'
+import draggable from 'vuedraggable'
 
 export default defineComponent({
   name: 'Home',
+  methods: {
+    ...mapActions([
+      'insertBoard',
+      'insertTask',
+    ]),
+  },
+  computed: {
+    ...mapGetters([
+      'boards',
+    ])
+  },
   components: {
-    HelloWorld,
+    draggable: draggable,
   },
 });
 </script>
+<style lang="scss">
+.board {
+  &__title {
+    margin: 10px 0;
+  }
+  &__list {
+    display: flex;
+    gap: 40px;
+    height: calc(100vh - 140px);
+  }
+  &__item {
+    width: 300px;
+    max-height: 100vh;
+    padding: 16px;
+    background: #f6f8fc;
+    box-shadow: 0 0 4px #0003;
+    flex-shrink: 0;
+    flex-basis: 300px;
+  }
+  &__bottom {
+    padding-top: 20px;
+    text-align: center;
+  }
+  &__link {
+    text-decoration: none;
+    color: #2a2a2a;
+    font-weight: 600;
+    font-size: 14px;
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+}
+.task {
+  background: #ffffff;
+  padding: 14px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #0003;
+  user-select: none;
+
+  &__title {
+    margin: 10px 0;
+    font-weight: 200;
+    font-size: 16px;
+    color: #393939;
+  }
+}
+.marker {
+  &__list {
+    display: flex;
+  }
+  &__item {
+    padding: 6px 12px;
+    background: linear-gradient(to bottom, #00b4db, #0083b0);
+    color: #fff;
+    font-size: 11px;
+    letter-spacing: 1px;
+    border-radius: 4px;
+    box-shadow: 0 0 4px #0008;
+  }
+}
+.box {
+  &__right {
+    width: 300px;
+    flex-shrink: 0;
+    flex-basis: 300px;
+  }
+  &__add {
+    $color_bg: #e8e9ed;
+
+    background: $color_bg;
+    padding: 10px 20px;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 4px;
+    color: #2c2c2c;
+    font-weight: 400;
+    cursor: pointer;
+    user-select: none;
+
+    &:hover {
+      background: darken($color_bg, 10%);
+    }
+    &:active {
+      box-shadow: 0 0 3px #0004;
+    }
+  }
+}
+.draggable {
+  min-height: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  overflow-x: auto;
+  max-height: calc(100% - 120px);
+  padding: 20px 4px;
+}
+</style>
